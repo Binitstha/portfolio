1ef17f6209009aa4ef0f1b0d2b8e5e63c5d4c0b1
Binit shrestha <binitcr336@gmail.com>
2023-07-01 20:20:15 +0545

    uploading projects

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const list = [
         projectName: "Calculator",
         className: "fa-solid fa-calculator",
         discription: "The project provides a straightforward and efficient way to build a functional calculator using JavaScript and the eval keyword. It supports addition, subtraction, multiplication, and division operations, enabling users to perform calculations with ease.",
-        link: "Calculator\index.html",
+        link: "Calculator/index.html",
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ const list = [
         projectName: "Stopwatch",
         className: "fa-solid fa-stopwatch",
         discription: "The project provides a simple yet effective way to implement a stopwatch functionality in JavaScript using the Date.now() method to accurately measure time.The stopwatch features start, stop,and reset functionalities, allowing users to control the timing accurately.",
-        link: "StopWatch\a1.html",
+        link: "StopWatch/index.html",
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ const list = [
         projectName: "Tik tak toe",
         className: "fa-solid fa-gamepad",
         discription: "The project includes logic to check for a winning combination, which occurs when a player successfully forms a line of their symbols horizontally, vertically, or diagonally. When all cells on the board are filled without a winning combination game will be ended as tie.",
-        link: "tik tak toe\index.html",
+        link: "tik tak toe/index.html",
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ const list = [
         projectName: "Rock paper scissors",
         className: "fa-solid fa-hands",
         discription: "The project includes logic to compare the player's choice with the computer's choice and display the outcome on the screen, indicating whether the player wins, loses, or it's a tie.This JavaScript project offers an enjoyable and interactive way to play game directly in the browser.",
-        link: "Rock Paper Scissors/Index.html",
+        link: "Rock Paper Scissors/index.html",
     },
 ]
 const project = document.querySelector(".project")
