a7ff7e1afcd8e7e77b0baa3725a54cb9f21e5a00
Binitstha <binitcr336@gmail.com>
2024-10-27 00:46:07 +0545

    feat: project live links and source code link added with images

--- src/components/Ui/projectCard.tsx
+++ src/components/Ui/projectCard.tsx
@@ -1,44 +1,74 @@
+"use client";
 import { soraBold, soraLightBold } from "@/app/fonts/fonts";
 import Image from "next/image";
 import Link from "next/link";
+import { BsArrowRight } from "react-icons/bs";
 import { HiOutlineExternalLink } from "react-icons/hi";
 
 interface ProjectCardProps {
   reverse: boolean;
   title: string;
   description: string;
+  image: string;
+  link: string;
+  gitHubLink: string;
+  projectNumber: string;
 }
 
-const ProjectCard = ({ reverse, title, description }: ProjectCardProps) => {
+const ProjectCard = ({
+  reverse,
+  projectNumber,
+  gitHubLink,
+  link,
+  image,
+  title,
+  description,
+}: ProjectCardProps) => {
   return (
     <section
       className={
         reverse
           ? "flex flex-row-reverse justify-center items-center gap-10"
-          : "flex justify-center items-center gap-16"
+          : "flex justify-center items-center gap-16 "
       }
     >
-      <figure className="overflow-hidden flex-[35%] rounded-xl">
+      <figure className="overflow-hidden flex-[35%] rounded-xl z-[-10px]">
         <Image
           alt="Project showcase"
-          src={"/wallpaper1.jpg"}
+          src={`/${image}`}
           width={1000}
           height={1000}
           unoptimized
-        //   className="w-[40rem] h-auto"
+          onContextMenu={(e) => e.preventDefault()}
+          className="z-[-1px]"
         />
         <figcaption className="sr-only">{title} - Project image</figcaption>
       </figure>
 
       <div className="flex flex-col justify-center flex-[45%] items-start gap-6">
-        <h1 className={soraBold.className + " text-4xl"}>01</h1>
+        <h1 className={soraBold.className + " text-4xl"}>{projectNumber}</h1>
         <h2 className={soraLightBold.className + " text-3xl font-extralight"}>
           {title}
         </h2>
-        <p>{description}</p>
-        <Link href={"#"} aria-label={`Learn more about ${title}`}>
-          <HiOutlineExternalLink className="text-2xl" />
-        </Link>
+        <p className="text-neutral-300">{description}</p>
+        <div className="flex gap-5 justify-between w-full items-center">
+          <Link
+            href={`${link}`}
+            aria-label={`Learn more about ${title}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <HiOutlineExternalLink className="text-2xl" />
+          </Link>
+          <Link
+            href={gitHubLink}
+            className="flex gap-2 items-center"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>Source code </span> <BsArrowRight />
+          </Link>
+        </div>
       </div>
     </section>
   );
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -16,24 +16,40 @@ const Projects = () => {
         </h1>
         <div className="flex mx-5 flex-wrap justify-between gap-16 items-center ">
           <ProjectCard
+            gitHubLink="https://github.com/Binitstha/Fitness-tracker"
+            projectNumber="01"
+            link="https://fitness-tracker-omega-liard.vercel.app/"
+            image="project1.png"
             reverse={false}
-            description="loremsakduishaiudhiohas iuahdoi ashiodhjoasij doisaj ofhasoihfd oiasjodi jasiohjd oasj odhjaso loremsakduishaiudhiohas iuahdoi ashiodhjoasij doisaj ofhasoihfd oiasjodi jasiohjd oasj odhjaso loremsakduishaiudhiohas iuahdoi ashiodhjoasij doisaj ofhasoihfd oiasjodi jasiohjd oasj odhjaso loremsakduishaiudhiohas iuahdoi ashiodhjoasij doisaj ofhasoihfd oiasjodi jasiohjd oasj odhjaso"
-            title="skjndans odsaj odsaoj dsajd"
+            title="Horizon Cloud: Streamlined Cloud Management"
+            description="Horizon Cloud is a comprehensive web application designed for efficient cloud resource management. Built with Next.js for server-side rendering and seamless navigation, it utilizes Firebase for real-time data management and NextAuth for secure user authentication, including OAuth support for various providers. The application features a modern user interface styled with Tailwind CSS and Material-UI (MUI) components for enhanced usability. Users can easily manage their cloud resources while enjoying a responsive and intuitive experience."
           />
           <ProjectCard
+            gitHubLink="https://github.com/Binitstha/Horizon-cloud"
+            projectNumber="02"
+            link="https://horizon-cloud-gamma.vercel.app/"
+            image="project4.jpg"
             reverse={true}
-            description="loremsakduishaiudhiohas iuahdoi ashiodhjoasij doisaj ofhasoihfd oiasjodi jasiohjd oasj odhjaso"
-            title="skjndans odsaj odsaoj dsajd"
+            description="The Fitness Tracker is a full-stack web application that helps users monitor their fitness journey. Built with React and Next.js for the front end and Node.js and Express for the back end, it uses MySQL with Prisma for data management. The app features responsive styling with Tailwind CSS, state management via React Context, and form handling through React Hook Form and Zod for validation. User authentication is secured with JWT (JSON Web Tokens). Key functionalities include workout logging and progress tracking through visual graphs."
+            title="Fitness Tracker: Monitor Your Progress"
           />
           <ProjectCard
+            gitHubLink="https://github.com/Binitstha/GoalPulse"
+            projectNumber="03"
+            link="https://goal-pulse-alpha.vercel.app/"
+            image="project3.png"
             reverse={false}
-            description="loremsakduishaiudhiohas iuahdoi ashiodhjoasij doisaj ofhasoihfd oiasjodi jasiohjd oasj odhjaso"
-            title="skjndans odsaj odsaoj dsajd"
+            description="GoalPulse is a web application that provides real-time information about the top five European football leagues. Built with Next.js for optimal performance and server-side rendering, this project fetches data from SerpAPI to deliver insights into club statistics, match highlights, and more. The application features a clean and intuitive interface styled with Tailwind CSS, allowing users to easily explore league standings, match results, and club information without the need for user authentication."
+            title="GoalPulse: Real-Time League Insights"
           />
           <ProjectCard
+            gitHubLink="https://github.com/Binitstha/sorting-visualizer"
+            projectNumber="04"
+            link="https://sorting-visualizer-rho-beige.vercel.app/"
+            image="project2.png"
             reverse={true}
-            description="loremsakduishaiudhiohas iuahdoi ashiodhjoasij doisaj ofhasoihfd oiasjodi jasiohjd oasj odhjaso"
-            title="skjndans odsaj odsaoj dsajd"
+            description="The Sorting Visualizer is an interactive tool designed to illustrate various sorting algorithms, including Bubble Sort, Selection Sort, and Quick Sort. Built with Next.js and utilizing the Context Hook for state management, it allows users to customize array sizes and animation speeds, providing real-time visualizations of the sorting process. This web-based application enhances understanding of algorithm mechanics, making it an invaluable resource for students and educators in computer science. By bridging the gap between theory and practice, the Sorting Visualizer fosters engagement and deeper comprehension of sorting concepts."
+            title="Sorting Visualizer: Visualizing the Mechanics of Sorting Algorithms"
           />
         </div>
       </section>
