9f579803216416b071dc063e775b3ad4f936343e
Binitstha <binitcr336@gmail.com>
2024-11-06 00:07:36 +0545

    fix: Enhanced metadata descriptions and keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,18 @@ import Footer from "@/components/footer";
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Binit Shrestha's portfolio",
+  title: "Binit Shrestha | Full Stack Developer Portfolio",
+  description:
+    "Explore the portfolio of Binit Shrestha, a full-stack developer with expertise in building modern, scalable web applications. Specializing in front-end and back-end development using React, Node.js, Next.js, JavaScript, and more.",
+  keywords:
+    "Binit Shrestha, full-stack developer, web developer, portfolio, front-end development, back-end development, React, Node.js, JavaScript, Next.js, web applications, REST APIs, database management, UI/UX design",
+  authors: [
+    {
+      name: "Binit Shrestha",
+      url: "https://binitshrestha7.com.np",
+    },
+  ],
+  robots: "index, follow",
 };
 
 export default async function RootLayout({
@@ -16,10 +26,10 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${sora.className} antialiased`}>
-          <NavBar />
-          {children}
-          <Footer />
-          <Toaster />
+        <NavBar />
+        {children}
+        <Footer />
+        <Toaster />
       </body>
     </html>
   );
